928ff86f21dd758bac6ff8d43054085544b73c99
idresult <vvsh@idresult.ru>
2018-11-24 09:53:54 +0300

    Отключение WP-cron
    Отключение WP-cron

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -79,6 +79,10 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+/** Отключаем wp-cron **/
+
+define('DISABLE_WP_CRON', 'true');
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
